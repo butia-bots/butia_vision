b5418bbe8bd97010794e17ae5b70b697cd308254
Miguel dos Santos <miguelmartinsdossantos@gmail.com>
2018-09-01 02:15:22 -0300

    Image server first drop.

--- img_server/src/img_server_node.cpp
+++ img_server/src/img_server_node.cpp
@@ -1,24 +1,33 @@
-#include "img_server_cfg.hpp"
+#include <ros/ros.h>
+#include <opencv2/opencv.hpp>
+#include <vector>
 
+#include "sensor_msgs/Image.h"
+#include "image_transport/image_transport.h"
+#include "cv_bridge/cv_bridge.h"
+#include "vision_system_msgs/ImageRequest.h"
+
+
+std::vector<sensor_msgs::ImageConstPtr> buffer;
+
+
+bool accessQueue(vision_system_msgs::ImageRequest::Request &req, vision_system_msgs::ImageRequest::Response &res) {
+    res.image = *(buffer[(req.frame)%150]);
+    return true;
+}
+
+
+void camCallBack(const sensor_msgs::ImageConstPtr img) {
+    buffer[(img->header.seq)%150] = img;
+}
 
 
 int main(int argc, char **argv) {
-    ImgServer buf;
-
-    ros::init(argc, argv, "img_server");
-
-    cv::namedWindow("Image", cv::WINDOW_AUTOSIZE);
-    cv::startWindowThread();
-
-    int i = 0;
-
-
-    while (ros::ok()) {
-        cv::imshow("Image", buf.getImage(i));
-        i++;
-        if (i >= 150)
-            i = 0;
-    }
-
-    cv::destroyAllWindows();
+    buffer.resize(150);
+    ros::init(argc, argv, "img_server_node");
+    ros::NodeHandle nh;
+    image_transport::ImageTransport it(nh);
+    image_transport::Subscriber img_sub = it.subscribe("usb_cam/image_raw", 100, &camCallBack);
+    ros::ServiceServer service = nh.advertiseService("/vision_system/img_server/image_request", &accessQueue);
+    ros::spin();
 }
