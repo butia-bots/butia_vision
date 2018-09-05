a421933e1793b9be369cbd8a4c525919e12e2d1f
Miguel dos Santos <miguelmartinsdossantos@gmail.com>
2018-09-05 12:54:39 -0300

    Image cropper created.

--- people_tracking/include/people_tracking.hpp
+++ people_tracking/include/people_tracking.hpp
@@ -1,31 +1,49 @@
 #include <ros/ros.h>
 #include <opencv2/opencv.hpp>
 #include <vector>
+#include <string>
 
 #include "sensor_msgs/Image.h"
 #include "cv_bridge/cv_bridge.h"
 #include "image_transport/image_transport.h"
 #include "vision_system_msgs/RecognizedPeople.h"
 #include "vision_system_msgs/ImageRequest.h"
+#include "vision_system_msgs/BoundingBox.h"
 
 
 
 
 class ImagePreparer {
     private:
-        ros::Subscriber reco_people_sub;
-        ros::ServiceClient img_server_client;
         vision_system_msgs::ImageRequest srv;
-        std::vector<sensor_msgs::Image> local_buffer;
+        std::vector<cv::Mat> local_server;
+        int imagenum;
 
     public:
-        ImagePreparer(ros::NodeHandle nh) {
-            reco_people_sub = nh.subscribe("/vision_system/or/people_recognition", 100, &this->peopleRecoCallBack, this);
-            img_server_client = nh.serviceClient<vision_system_msgs::ImageRequest>("/vision_system/img_server/image_request");
-        }
+        ImagePreparer() {imagenum = 0;}
 
         void peopleRecoCallBack(const vision_system_msgs::RecognizedPeopleConstPtr person) {
             srv.request.frame = person->image_header.seq;
-            local_buffer.push_back(srv.response.image);
+            for (int i = 0; i < person->people_description.size(); i++)
+                crop(srv.response.image, person->people_description[i].bounding_box);
+        }
+
+
+        void crop(sensor_msgs::Image image, vision_system_msgs::BoundingBox bounding_box) {
+            cv::Mat initial_image = (cv_bridge::toCvCopy(image, image.encoding))->image;
+
+            cv::Rect roi;
+            roi.x = bounding_box.width;
+            roi.y = bounding_box.height;
+            roi.width = initial_image.size().width - (roi.x * 2);
+            roi.height = initial_image.size().height - (roi.y * 2);
+
+            cv::Mat final_image = initial_image(roi);
+            local_server.push_back(final_image);
+
+            std::string image_name = std::to_string(imagenum) + ".pgm";
+            cv::imwrite(image_name, final_image);
+
+            imagenum++;
         }
 };
--- people_tracking/src/people_tracking_node.cpp
+++ people_tracking/src/people_tracking_node.cpp
@@ -5,5 +5,10 @@
 int main(int argc, char **argv) {
     ros::init(argc, argv, "people_tracking_node");
     ros::NodeHandle nh;
+
+    ImagePreparer preparer;
+    ros::Subscriber reco_people_sub = nh.subscribe("/vision_system/or/people_recognition", 100, &ImagePreparer::peopleRecoCallBack, &preparer);
+    ros::ServiceClient img_server_client = nh.serviceClient<vision_system_msgs::ImageRequest>("/vision_system/img_server/image_request");
+    
     ros::spin();
 }
