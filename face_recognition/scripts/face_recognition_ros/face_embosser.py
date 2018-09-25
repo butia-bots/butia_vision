266ad70ff41081886847a1830dbf8cbd2f6eb35f
Igor Maurell <igormaurell@furg.br>
2018-09-25 09:02:58 -0300

    adding degub decorator in embosser loader

--- face_recognition/scripts/face_recognition_ros/face_embosser.py
+++ face_recognition/scripts/face_recognition_ros/face_embosser.py
@@ -4,6 +4,7 @@ import openface
 import os
 
 @load(lib_name='FaceNet')
+@debug
 def loadFacenetModels(models_dir, model='nn4.small2.v1.t7', image_dimension=96, cuda=True, debug=False):
     facenet_embosser = openface.TorchNeuralNet(os.path.join(models_dir, 'openface', model), image_dimension, cuda=cuda)
     return facenet_embosser
