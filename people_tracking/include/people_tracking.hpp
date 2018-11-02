0b1adae850404c92bd597697fb81730a22a8e57d
Miguel <miguelmartinsdossantos@gmail.com>
2018-11-01 23:07:23 -0300

    People tracking tested without publishing positions.

--- people_tracking/include/people_tracking.hpp
+++ people_tracking/include/people_tracking.hpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <string>
 #include <utility>
+#include <iostream>
 
 #include "opencv2/xfeatures2d.hpp"
 #include "sensor_msgs/Image.h"
@@ -28,52 +29,62 @@ class PeopleTracker {
         ros::ServiceClient image_request_client; //Service that gets the RGBD image
         ros::ServiceClient image_segmentation_client; //Service that segments the person
 
-        ros::ServiceServer start_service;
-        ros::ServiceServer stop_service;
+        ros::ServiceServer start_service; //Service that starts the tracking
+        ros::ServiceServer stop_service; //Service that stops the tracking
 
-        int image_size;
-        int frame_id;
+        int image_size; //Variable that stores the size of the camera image
 
-        std::vector<vision_system_msgs::Description> descriptions;
+        int frame_id; //Stores the frame id to request the image from server
+        std::vector<vision_system_msgs::Description> descriptions; //Stores the descriptions of the people detected
 
+        //Stores the threshold to know if is confiable
         float bounding_box_size_threshold;
         float probability_threshold;
 
+        //Stores the cv mat segmented images
         cv::Mat mat_rgb_segmented_image;
         cv::Mat mat_grayscale_segmented_image;
+        cv::Mat actual_better_segmented_image;
 
-        int min_hessian;
+        int min_hessian; //Stores the hessian threshold to create the SURF detector
+        cv::FlannBasedMatcher matcher; //Matcher
+
+        //Detectors
         cv::Ptr<cv::xfeatures2d::SURF> surf_detector;
         cv::Ptr<cv::xfeatures2d::SIFT> sift_detector;
-        cv::FlannBasedMatcher matcher;
 
-        cv::Mat descriptors;
-        cv::Mat actual_descriptors;
+        //Descriptors
+        std::vector<cv::Mat_<float>> descriptors;
+        cv::Mat_<float> actual_descriptors;
+        cv::Mat_<float> better_descriptors;
 
-        std::vector<cv::KeyPoint> keypoints;
+        std::vector<cv::KeyPoint> keypoints; //Keypoints
 
+        //Matches
         std::vector<cv::DMatch> matches;
+        int good_matches;
 
-        std::vector<cv::DMatch> good_matches;
-        std::vector<cv::DMatch> actual_good_matches;
-
-        std::vector<int> bad_matches;
-        std::vector<int> actual_bad_matches;
-
+        //Parameters
         std::string param_detector_type;
         std::string param_start_service;
         std::string param_stop_service;
         std::string param_people_detection_topic;
         std::string param_image_request_service;
         std::string param_segmentation_request_service;
-
         int param_k;
         float minimal_minimal_distance;
         float matches_check_factor;
+        int queue_size;
 
+        //Control variables
         bool initialized;
         bool person_founded;
-        int number_of_matches_on_better_match;
+
+        //Queue description variables
+        int queue_actual_size;
+        int actual_iterator;
+
+        int number_of_matches_on_better_match; //Stores the number of matches of better match
 
 
     public:
@@ -81,16 +92,16 @@ class PeopleTracker {
 
         void peopleDetectionCallBack(const vision_system_msgs::Recognitions::ConstPtr &person_detected); //CallBack
 
+        //Services' Functions
         bool startTracking(vision_system_msgs::StartTracking::Request &req, vision_system_msgs::StartTracking::Response &res);
         bool stopTracking(vision_system_msgs::StopTracking::Request &req, vision_system_msgs::StopTracking::Response &res);
+        
+        void readImage(const sensor_msgs::Image::ConstPtr &source, cv::Mat &destiny); //Function that reads the image
 
-        void readImage(const sensor_msgs::Image::ConstPtr &source, cv::Mat &destiny);
-        void extractFeatures(cv::Mat &descriptors_destiny);
-        bool matchFeatures();
+        //Feature matching functions  
+        void extractFeatures(cv::Mat_<float> &destiny);
+        bool matchFeatures(cv::Mat_<float> &destiny);
         void registerMatch();
 
-        void readParameters();
+        void readParameters(); //Function that reads the parameters
 };
-
-
-
