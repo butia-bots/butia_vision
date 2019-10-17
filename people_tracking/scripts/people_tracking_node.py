3e1a685b6e22c687ba566999d298eba8f2dc28f7
Lucas Avila <lucas.soares.avila@gmail.com>
2019-10-17 17:44:16 -0300

    Implemented tracking for all people in image

--- people_tracking/scripts/people_tracking_node.py
+++ people_tracking/scripts/people_tracking_node.py
@@ -2,12 +2,14 @@
 
 import cv2
 import rospy
+import os
+import rospkg
 
 from cv_bridge import CvBridge
 
-from people_tracking_ros import PeopleTracking
+from people_tracking_ros.PeopleTracking import PeopleTracking
 
-from butia_vision_msgs.msg import Recognition, Description, BoundingBox, RGBDImage
+from butia_vision_msgs.msg import Recognitions, Description, BoundingBox, RGBDImage
 from std_msgs.msg import Header
 from sensor_msgs.msg import Image
 
@@ -15,14 +17,9 @@ from butia_vision_msgs.srv import ImageRequest, SegmentationRequest, StartTracki
 
 BRIDGE = CvBridge()
 
-bounding_box_size_threshold=None
 probability_threshold=None
 segmentation_type=None
 queue_size=None
-min_hessian=None
-minimal_minimal_distance=None
-matches_check_factor=None
-param_k=None
 
 people_tracking=None
 
@@ -36,49 +33,49 @@ image_request_client=None
 segmentation_request_client=None
 
 frame=None
-cv_frame=None
 descriptions=None
 
-def debug(tracker, dets):
+PACK_DIR = rospkg.RosPack().get_path('people_tracking')
+
+def debug(cv_frame, tracker, dets):
     for track in tracker.tracks:
         if not track.is_confirmed() or track.time_since_update > 1:
-				continue
-        
+            continue
+
         bbox = track.to_tlbr()
         id_num= str(track.track_id)
         features = track.features
-        
+
         cv2.rectangle(cv_frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])),(255,255,255), 2)
-		cv2.putText(cv_frame, str(id_num),(int(bbox[0]), int(bbox[1])),0, 5e-3 * 200, (0,255,0),2)
+        cv2.putText(cv_frame, str(id_num),(int(bbox[0]), int(bbox[1])),0, 5e-3 * 200, (0,255,0),2)
 
         for det in dets:
             bbox = det.to_tlbr()
             cv2.rectangle(cv_frame,(int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])),(255,255,0), 2)
-    
+
     cv2.imshow('cv_frame',cv_frame)
-    out.write(cv_frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    cv2.waitKey(1)
     
 
 #Tracking people in real time
 def peopleDetectionCallBack(recognition):
-    frame_id = image_header.seq
+    frame_id = recognition.image_header.seq
     req = image_request_client(frame_id)
-    frame = req.rgbd
-    cv_frame = BRIDGE.imgmsg_to_cv2(frame.rgb, desired_encoding = 'rgb8')
-    people_tracking.setFrame(recognition.image_header, recognition.header, frame_id, cv_frame)
+    frame = req.rgbd_image.rgb
+    descriptions = recognition.descriptions
+    cv_frame = BRIDGE.imgmsg_to_cv2(frame, desired_encoding = 'rgb8')
+    frame_rgb = cv2.cvtColor(cv_frame,cv2.COLOR_BGR2RGB)
+    people_tracking.setFrame(recognition.image_header, recognition.header, frame_id, frame_rgb.copy())
 
-    img_size = frame.rgb.height * frame.rgb.width
-    for description in descriptions:
-        if(description.bounding_box.width*description.bounding_box.height < img_size*bounding_box_size_threshold or description.probability < probability_threshold):
-            descriptions.remove(description)
+    img_size = frame.height * frame.width
     
-    people_tracking.generateDetections(descriptions)
+    for description in descriptions:
+        if(description.probability < probability_threshold):
+            descriptions.remove(description)
 
-    tracker, detections = people_tracking.track()
+    tracker, detections = people_tracking.track(descriptions)
 
-    debug(people_tracking.tracker, people_tracking.dets)
+    debug(frame_rgb.copy(), tracker, detections)
 
     '''
     if(people_tracking.tracking) {
@@ -90,7 +87,7 @@ def peopleDetectionCallBack(recognition):
 
 
 def startTracking(start):
-    #people_tracking.startTrack()
+    people_tracking.startTrack()
     return start
 
 def stopTracking(stop):
@@ -100,29 +97,19 @@ def stopTracking(stop):
 if __name__ == '__main__':
     rospy.init_node('people_tacking_node', anonymous = True)
     
-    bounding_box_size_threshold = rospy.get_param("/people_tracking/thresholds/bounding_box_size", 0.1)
     probability_threshold = rospy.get_param("/people_tracking/thresholds/probability", 0.5)
-    segmentation_type = rospy.get_param("/people_tracking/segmentation/type", "median_full")
     
     queue_size = rospy.get_param("/people_tracking/queue/size", 20)
-
-    min_hessian = rospy.get_param("/people_tracking/match/minimal_hessian", 400)
-    minimal_minimal_distance = rospy.get_param("/people_tracking/match/minimal_minimal_distance", 0.2)
-    matches_check_factor = rospy.get_param("/people_tracking/match/check_factor", 0.2)
-    param_k = rospy.get_param("/people_tracking/match/k", 8)
-
-    param_detector_type = rospy.get_param("/people_tracking/detector/type", "surf")
 
     param_start_service = rospy.get_param("/services/people_tracking/start_tracking", "/butia_vision/pt/start")
     param_stop_service = rospy.get_param("/services/people_tracking/stop_tracking", "/butia_vision/pt/stop")
     param_image_request_service = rospy.get_param("/services/image_server/image_request", "/butia_vision/is/image_request")
-    param_segmentation_request_service = rospy.get_param("/services/segmentation/segmentation_request", "/butia_vision/seg/image_segmentation")
 
     param_people_detection_topic = rospy.get_param("/topics/object_recognition/people_detection", "/butia_vision/or/people_detection")
     param_people_tracking_topic = rospy.get_param("/topics/people_tracking/people_tracking", "/butia_vision/pt/people_tracking")
 
-    tracker_publisher = rospy.Publisher(param_people_tracking_topic, Recognition, queue_size = 10)
-    tracker_subscriber = rospy.Subscriber(param_people_detection_topic, Recognition, peopleDetectionCallBack, queue_size = 10)
+    tracker_publisher = rospy.Publisher(param_people_tracking_topic, Recognitions, queue_size = 10)
+    tracker_subscriber = rospy.Subscriber(param_people_detection_topic, Recognitions, peopleDetectionCallBack, queue_size = 10)
 
     start_service = rospy.Service(param_start_service, StartTracking, startTracking)
     stop_service = rospy.Service(param_stop_service, StopTracking, stopTracking)
@@ -132,14 +119,11 @@ if __name__ == '__main__':
         image_request_client = rospy.ServiceProxy(param_image_request_service, ImageRequest)
     except rospy.ServiceException, e:
         print "Service call failed %s"%e
-
-    rospy.wait_for_service(param_segmentation_request_service)
-    try:
-        segmentation_request_client = rospy.ServiceProxy(param_segmentation_request_service, SegmentationRequest)
-    except rospy.ServiceException, f:
-        print "Service call failed %s"%e 
     
-    people_tracking = PeopleTracking()
+    models_dir = os.path.join(PACK_DIR, 'models')
+    model_file = os.path.join(models_dir, 'mars-small128.pb')
+
+    people_tracking = PeopleTracking(model_file)
 
     rospy.spin()
     
--- people_tracking/scripts/people_tracking_ros/PeopleTracking.py
+++ people_tracking/scripts/people_tracking_ros/PeopleTracking.py
@@ -1,54 +1,57 @@
 #!/usr/bin/env python
 
-from deep_sort.deep_sort import nn_matching
-from deep_sort.deep_sort.tracker import Tracker 
-from deep_sort.application_util import preprocessing as prep
-from deep_sort.application_util import visualization
-from deep_sort.deep_sort.detection import Detection
+from deep_sort import nn_matching
+from deep_sort.tracker import Tracker 
+from deep_sort import preprocessing as prep
+from deep_sort import visualization
+from deep_sort.detection import Detection
+from deep_sort import generate_detections
 
-from butia_vision_msgs.msg import Recognition, Description, BoundingBox
+from butia_vision_msgs.msg import Recognitions, Description, BoundingBox
 from std_msgs.msg import Header
 from sensor_msgs.msg import Image
 
 import cv2
+import os
 
 import numpy as np
 
 class PeopleTracking():
     
-    def __init__(self, feature_generator):
-        self.encoder = generate_detections.create_box_encoder("deep_sort/resources/mars-small128.ckpt-68577")	
+    def __init__(self, model_path):
+        self.encoder = generate_detections.create_box_encoder(os.path.abspath(model_path))
         self.metric = nn_matching.NearestNeighborDistanceMetric("cosine", .5, 100)
         self.tracker = Tracker(self.metric)
         self.detections = []
 
-    def setFrame(image_header, header, frame_id, frame)
+    def setFrame(self, image_header, header, frame_id, frame):
         self.image_header = image_header
         self.header = header
         self.frame_id = frame_id
         self.frame = frame
-
-    def generateDetections(descriptions):
-        frame_bgr = cv2.cvtColor(frame, cv2.RGB2BGR)
-        for i in range(len(descriptions)):
-            bbox = (description[i].minX, description[i].minY, description[i].width, description[i].height)
-            features = self.encoder(frame_bgr, [bbox])   
-            self.detections.append(Detection(bbox, description[i].probability, features[0]))
         
-    def track():
+    def track(self, descriptions):
         
-        if self.detections == []:
+        if descriptions == []:
             self.tracker.predict()
             print("No detections")
             trackers = self.tracker.tracks 
-            return trackers
-        
-        outboxes = np.array([self.detections.tlwh for d in self.detections])
-        outscores = np.array([self.detections.confidence for d in self.detections])
+            return trackers, None
 
-        indices = prep.non_max_suppression(outboxes, 0.8, outscores)
+        detections = np.array([(description.bounding_box.minX, description.bounding_box.minY, description.bounding_box.width, description.bounding_box.height) for description in descriptions])
 
-        dets = [self.detections[i] for i in indices]
+        out_scores = [description.probability for description in descriptions]
+
+        features = self.encoder(self.frame, detections)
+
+        dets = [Detection(bbox, score, feature) for bbox, score, feature in zip(detections, out_scores, features)]
+
+        outboxes = np.array([d.tlwh for d in dets])
+        outscores = np.array([d.confidence for d in dets])
+
+        indices = prep.non_max_suppression(outboxes, 0.5, outscores)
+
+        dets = [dets[i] for i in indices]
 
         self.tracker.predict()
         self.tracker.update(dets)
