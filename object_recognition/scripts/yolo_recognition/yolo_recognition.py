3e560e22f31fc8dbd85246d812a8b81391e907ec
Igor Maurell <igormaurell@furg.br>
2022-06-30 18:21:31 -0300

    changing some names and order of parameters

--- object_recognition/scripts/yolo_recognition/yolo_recognition.py
+++ object_recognition/scripts/yolo_recognition/yolo_recognition.py
@@ -16,81 +16,11 @@ from butia_vision_msgs.srv import ListClasses, ListClassesResponse
 
 torch.set_num_threads(1)
 
-dictionary={"person_standing": "Person",
-            "ycb_002_master_chef_can": "master_chef_can", 
-            "ycb_003_cracker_box": "cracker_box", 
-            "ycb_004_sugar_box": "sugar_box", 
-            "ycb_005_tomato_soup_can": "tomato_soup_can", 
-            "ycb_006_mustard_bottle": "mustard_bottle", 
-            "ycb_007_tuna_fish_can": "tuna_fish_can", 
-            "ycb_008_pudding_box": "pudding_box", 
-            "ycb_009_gelatin_box": "gelatin_box", 
-            "ycb_010_potted_meat_can": "potted_meat_can", 
-            "ycb_011_banana": "banana", 
-            "ycb_012_strawberry": "strawberry", 
-            "ycb_013_apple": "apple", 
-            "ycb_014_lemon": "lemon", 
-            "ycb_015_peach": "peach", 
-            "ycb_016_pear": "pear", 
-            "ycb_017_orange": "orange", 
-            "ycb_018_plum": "plum", 
-            "ycb_019_pitcher_base": "Pitcher base", 
-            "ycb_021_bleach_cleanser": "Srub cleanser bottle", 
-            "ycb_022_windex_bottle": "Windex Spray bottle", 
-            "ycb_024_bowl": "Bowl", 
-            "ycb_025_mug": "Mug", 
-            "ycb_026_sponge": "Scotch brite dobie sponge", 
-            "ycb_029_plate": "Plate", 
-            "ycb_030_fork": "Fork", 
-            "ycb_031_spoon": "Spoon", 
-            "ycb_033_spatula": "Spatula", 
-            "ycb_040_large_marker": "Large marker", 
-            "ycb_051_large_clamp": "Clamps", 
-            "ycb_053_mini_soccer_ball": "Mini soccer ball", 
-            "ycb_054_softball": "Soft ball", 
-            "ycb_055_baseball": "Baseball", 
-            "ycb_056_tennis_ball": "Tennis ball", 
-            "ycb_057_racquetball": "Racquetball", 
-            "ycb_058_golf_ball": "Golf ball", 
-            "ycb_059_chain": "Chain", 
-            "ycb_061_foam_brick": "Foam brick", 
-            "ycb_062_dice": "Dice", 
-            "ycb_063-a_marbles": "Marbles", 
-            "ycb_063-b_marbles": "Marbles", 
-            "ycb_065-a_cups": "Cups", 
-            "ycb_065-b_cups": "Cups", 
-            "ycb_065-c_cups": "Cups", 
-            "ycb_065-d_cups": "Cups", 
-            "ycb_065-e_cups": "Cups", 
-            "ycb_065-f_cups": "Cups", 
-            "ycb_065-g_cups": "Cups", 
-            "ycb_065-h_cups": "Cups", 
-            "ycb_065-i_cups": "Cups", 
-            "ycb_065-j_cups": "Cups", 
-            "ycb_070-a_colored_wood_blocks": "Colored wood blocks", 
-            "ycb_070-b_colored_wood_blocks": "Colored wood blocks", 
-            "ycb_071_nine_hole_peg_test": "9-peg-hole test", 
-            "ycb_072-a_toy_airplane": "Toy airplane", 
-            "ycb_072-b_toy_airplane": "Toy airplane", 
-            "ycb_072-c_toy_airplane": "Toy airplane", 
-            "ycb_072-d_toy_airplane": "Toy airplane", 
-            "ycb_072-e_toy_airplane": "Toy airplane", 
-            "ycb_073-a_lego_duplo": "Lego duplo", 
-            "ycb_073-b_lego_duplo": "Lego duplo", 
-            "ycb_073-c_lego_duplo": "Lego duplo", 
-            "ycb_073-d_lego_duplo": "Lego duplo", 
-            "ycb_073-e_lego_duplo": "Lego duplo", 
-            "ycb_073-f_lego_duplo": "Lego duplo", 
-            "ycb_073-g_lego_duplo": "Lego duplo", 
-            "ycb_077_rubiks_cube": "Rubik’s cube"
-}
-
-colors = dict([(k, np.random.randint(low=0, high=256, size=(3,)).tolist()) for k,v in dictionary.items()])
-
-
 class YoloRecognition():
     def __init__(self):
         self.readParameters()
+
+        self.colors = dict([(k, np.random.randint(low=0, high=256, size=(3,)).tolist()) for k in self.all_classes])
 
         self.rospack = rospkg.RosPack()
         print(self.rospack.get_path('object_recognition'))
@@ -131,7 +61,7 @@ class YoloRecognition():
     
     def getObjectList(self, req):
         objects_list = []
-        for key, value in self.possible_classes.items():
+        for key, value in self.classes_by_category.items():
             for elem in value:
                 objects_list.append(key + '/' + elem)
         return ListClassesResponse(objects_list)
@@ -161,9 +91,9 @@ class YoloRecognition():
                     reference_model = bbs_l['name'][i]
                     print(bbs_l['name'][i])
                     cv_img = cv2.cvtColor(cv_img, cv2.COLOR_RGB2BGR)
-                    cv_img = cv2.rectangle(cv_img, (int(bbs_l['xmin'][i]), int(bbs_l['ymin'][i])), (int(bbs_l['xmax'][i]), int(bbs_l['ymax'][i])), colors[bbs_l['name'][i]])
-                    cv_img = cv2.putText(cv_img, bbs_l['name'][i], (int(bbs_l['xmin'][i]), int(bbs_l['ymin'][i])), cv2.FONT_HERSHEY_SIMPLEX, 1.0, color=colors[bbs_l['name'][i]])
-                    if 'people' in self.possible_classes and bbs_l['name'][i] in self.possible_classes['people'] and bbs_l['confidence'][i] >= self.threshold:
+                    cv_img = cv2.rectangle(cv_img, (int(bbs_l['xmin'][i]), int(bbs_l['ymin'][i])), (int(bbs_l['xmax'][i]), int(bbs_l['ymax'][i])), self.colors[bbs_l['name'][i]])
+                    cv_img = cv2.putText(cv_img, bbs_l['name'][i], (int(bbs_l['xmin'][i]), int(bbs_l['ymin'][i])), cv2.FONT_HERSHEY_SIMPLEX, 1.0, color=self.colors[bbs_l['name'][i]])
+                    if 'people' in self.classes_by_category and bbs_l['name'][i] in self.classes_by_category['people'] and bbs_l['confidence'][i] >= self.threshold:
                         person = Description()
                         person.label_class = 'people' + '/' + bbs_l['name'][i]
                         person.reference_model = reference_model
@@ -174,16 +104,16 @@ class YoloRecognition():
                         person.bounding_box.height = int(bbs_l['ymax'][i] - bbs_l['ymin'][i])
                         people.append(person)
 
-                    elif bbs_l['name'][i] in [val for sublist in self.possible_classes.values() for val in sublist] and bbs_l['confidence'][i] >= self.threshold:
+                    elif bbs_l['name'][i] in [val for sublist in self.classes_by_category.values() for val in sublist] and bbs_l['confidence'][i] >= self.threshold:
                         object_d = Description()
                         index = 0
                         j = 0
-                        for value in self.possible_classes.values():
+                        for value in self.classes_by_category.values():
                             if bbs_l['name'][i] in value:
                                 index = j
                             j += 1
                         object_d.reference_model = reference_model
-                        object_d.label_class = list(self.possible_classes.keys())[index] + '/' + bbs_l['name'][i]
+                        object_d.label_class = list(self.classes_by_category.keys())[index] + '/' + bbs_l['name'][i]
                         object_d.probability = bbs_l['confidence'][i]
                         object_d.bounding_box.minX = int(bbs_l['xmin'][i])
                         object_d.bounding_box.minY = int(bbs_l['ymin'][i])
@@ -211,8 +141,6 @@ class YoloRecognition():
                     people_msg.image_header = img.header
                     people_msg.descriptions = people
                     self.recognized_people_pub.publish(people_msg)
-            
-
 
     def readParameters(self):
         self.bounding_boxes_topic = rospy.get_param("/object_recognition/subscribers/bounding_boxes/topic", "/darknet_ros/bounding_boxes")
@@ -238,9 +166,9 @@ class YoloRecognition():
         self.stop_service = rospy.get_param("/object_recognition/servers/stop/service", "/butia_vision/or/stop")
 
         self.threshold = rospy.get_param("/object_recognition/threshold", 0.5)
-        
-        self.possible_classes = dict(rospy.get_param("/object_recognition/possible_classes"))
 
         self.all_classes = list(rospy.get_param("/object_recognition/all_classes"))
+
+        self.classes_by_category = dict(rospy.get_param("/object_recognition/classes_by_category"))
 
         self.model_file = rospy.get_param("/object_recognition/model_file", "larc2021_go_and_get_it.pt")
