09129e313c6ab17800150e3ea2ed69c7e5b341a5
Igor Maurell <igormaurell@furg.br>
2022-08-16 14:10:13 -0300

    butia_vision_bridge: new version implemented, but not tested yet

--- butia_vision_bridge/scripts/vision_bridge/vision_bridge.py
+++ butia_vision_bridge/scripts/vision_bridge/vision_bridge.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 import rospy
 
+import numpy as np
 import cv2
 
 from sensor_msgs.msg import Image, CameraInfo, PointCloud2
@@ -21,24 +22,58 @@ class VisionBridge:
     def __init__(self):
         self.readParameters()
 
-        self.createSubscribers()
+        self.createSubscribersAndPublishers()
     
     def processData(self, data: Image):
-        pass
+        image = np.frombuffer(data.data, dtype=np.uint8).reshape(data.height, data.width, -1)
+        image = cv2.resize(image, (self.width, self.height), self.INTER_LINEAR)
+
+        data.data = image[:,:,::-1].flatten().tolist()
+        data.width = self.width
+        data.height = self.height
+        return data
     
     def processData(self, data: CameraInfo):
-        pass
+        scale_x = self.width/data.width
+        scale_y = self.height/data.height
+
+        data.K[0] *= scale_x
+        data.K[2] *= scale_x
+        data.K[4] *= scale_y
+        data.K[5] *= scale_y
+
+        data.width = self.width
+        data.height = self.height
+
+        return data
     
     def processData(self, data: PointCloud2):
-        pass
+        pc = np.frombuffer(data.data, dtype=np.uint8).reshape(data.height, data.width, -1)
+        #since point cloud is organized, I am using opencv to resize it
+        pc = cv2.resize(pc, (self.width, self.height), self.INTER_LINEAR)
+
+        data.data = pc[:,:,::-1].flatten().tolist()
+        data.width = self.width
+        data.height = self.height
+        return data
 
     def callback(self, *args):
-        pass
+        publish_dict = {}
+        for i, data in enumerate(args):
+            source = self.sync_sources[i]
+            publish_dict[source] = self.processData(data)
+        self.publish(publish_dict)
+
+    def publish(self, data_dict):
+        for source, data in data_dict.values():
+            self.publishers[source].publish(data)
     
-    def createSubscribers(self):
+    def createSubscribersAndPublishers(self):
         self.subscribers = []
+        self.publishers = {}
         for source in self.sync_sources:
-            self.subscribers.append(message_filters.Subscriber(source, VisionBridge.SOURCE_MESSAGE_TYPES[source]))
+            self.subscribers.append(message_filters.Subscriber(source + '_sub', VisionBridge.SOURCE_MESSAGE_TYPES[source]))
+            self.publishers[source] = rospy.Publisher(source + '_pub', VisionBridge.SOURCE_MESSAGE_TYPES[source], queue_size=self.queue_size)
         if self.use_exact_sync:
             self.time_synchronizer = message_filters.TimeSynchronizer(self.subscribers, self.queue_size)
         else:
