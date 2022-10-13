b0b239b00a44b8ad61d39be1164e2d915270b741
Igor Maurell <igormaurell@furg.br>
2022-10-13 12:55:32 -0300

    fixing bug of sources order in vision synchronizer

--- butia_vision_bridge/scripts/butia_vision_bridge/vision_synchronizer/vision_synchronizer.py
+++ butia_vision_bridge/scripts/butia_vision_bridge/vision_synchronizer/vision_synchronizer.py
@@ -5,17 +5,21 @@ import message_filters
 from butia_vision_bridge import VisionBridge
 
 class VisionSynchronizer:
+    POSSIBLE_SOURCES = ['camera_info', 'image_rgb', 'image_depth', 'points']
+
     def syncSubscribers(source_topic_dict, callback, queue_size=1, exact_time=False, slop=0.1):
-        subscribers = {}
-        for source, topic in source_topic_dict.items():
-            subscribers[source] = message_filters.Subscriber(topic, VisionBridge.SOURCES_TYPES[source])
+        subscribers = []
+        for source in VisionSynchronizer.POSSIBLE_SOURCES:
+            if source in source_topic_dict.keys():
+            	subscribers.append((source, message_filters.Subscriber(source_topic_dict[source], VisionBridge.SOURCES_TYPES[source])))
         
         ts = None
         if exact_time:
-            ts = message_filters.TimeSynchronizer(subscribers.values(), queue_size=queue_size)
+            ts = message_filters.TimeSynchronizer([x[1] for x in subscribers], queue_size=queue_size)
         else:
-            ts = message_filters.ApproximateTimeSynchronizer(subscribers.values(), queue_size=queue_size, slop=slop)
+            print([x[0] for x in subscribers])
+            ts = message_filters.ApproximateTimeSynchronizer([x[1] for x in subscribers], queue_size=queue_size, slop=slop)
         ts.registerCallback(callback)
 
-        return subscribers
+        return dict(subscribers)
         
