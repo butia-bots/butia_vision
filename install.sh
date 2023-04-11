c4f598ea431cde1ff540b58d26e88e153926f47f
André Maurell <90858902+andremaurell@users.noreply.github.com>
2023-04-11 12:54:39 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 [ $(which git) ] || sudo apt install git
-pip3 install -r ./requirements.txt
+pip install -r ./requirements.txt
 
 mkdir -p ~/butia_ws/src
 cd ~/butia_ws/src
@@ -10,19 +10,20 @@ cd ~/butia_ws/src
 git clone https://github.com/butia-bots/iai_kinect2.git
 git clone https://github.com/butia-bots/libfreenect2.git
 
-#cd ~/butia_ws/src/butia_vision
+cd ~/butia_ws/src/butia_vision
+SHELL_TYPE=$(echo $SHELL | grep -oE '[^/]+/?$' | tr -d '/')
 
-#if [[ $SHELL_TYPE == "bash" ]]; then
-#    echo "export PYTHONPATH=/usr/local/lib/:$PYTHONPATH" >> ~/.bashrc
-#    echo "export CMAKE_PREFIX_PATH=/usr/local:$CMAKE_PREFIX_PATH" >> ~/.bashrc
-#    echo "export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH" >> ~/.bashrc
-#    source ~/.bashrc
-#elif [[ $SHELL_TYPE == "zsh" ]]; then
-#    echo "export PYTHONPATH=/usr/local/lib/:$PYTHONPATH" >> ~/.zshrc
-#    echo "export CMAKE_PREFIX_PATH=/usr/local:$CMAKE_PREFIX_PATH" >> ~/.zshrc
-#    echo "export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH" >> ~/.zshrc
-#    source ~/.zshrc
-#else
-#    echo "Não foi possível determinar o tipo de shell"
-#    exit
-#fi
+if [[ $SHELL_TYPE == "bash" ]]; then
+    echo "export PYTHONPATH=/usr/local/lib/:$PYTHONPATH" >> ~/.bashrc
+    echo "export CMAKE_PREFIX_PATH=/usr/local:$CMAKE_PREFIX_PATH" >> ~/.bashrc
+    echo "export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH" >> ~/.bashrc
+    source ~/.bashrc
+elif [[ $SHELL_TYPE == "zsh" ]]; then
+    echo "export PYTHONPATH=/usr/local/lib/:$PYTHONPATH" >> ~/.zshrc
+    echo "export CMAKE_PREFIX_PATH=/usr/local:$CMAKE_PREFIX_PATH" >> ~/.zshrc
+    echo "export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH" >> ~/.zshrc
+    source ~/.zshrc
+else
+    echo "Não foi possível determinar o tipo de shell"
+    exit
+fi
