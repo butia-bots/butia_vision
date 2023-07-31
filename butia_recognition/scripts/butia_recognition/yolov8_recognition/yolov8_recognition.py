6aaafa0797954f4bf217cb632221383cd9cdae0a
Jfsslemos <jfsslemos@gmail.com>
2023-07-31 13:51:05 -0300

    creating launchs for manip tests

--- butia_recognition/scripts/butia_recognition/yolov8_recognition/yolov8_recognition.py
+++ butia_recognition/scripts/butia_recognition/yolov8_recognition/yolov8_recognition.py
@@ -40,7 +40,7 @@ class YoloV8Recognition(BaseRecognition):
         return super().serverStop(req)
 
     def loadModel(self): 
-        self.model = YOLO("/home/jetson/sd/butia_ws/src/butia_vision/butia_recognition/config/yolov8_network_config/yolov8_lab_objects.pt")
+        self.model = YOLO("yolov8n.pt")
         self.model.conf = self.threshold
         print('Done loading model!')
 
