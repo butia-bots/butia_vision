ba2bb36393a3179179697f4e8e48913f621febfa
Eduardo Evangelista <eduardo.evangelista2002@gmail.com>
2024-10-17 08:29:48 -0300

    :construction: in progress: refactor msg

--- butia_recognition/scripts/butia_recognition/face_recognition/face_recog.py
+++ butia_recognition/scripts/butia_recognition/face_recognition/face_recog.py
@@ -13,7 +13,6 @@ import cv2
 import face_recognition
 import time
 import rospkg
-from collections import Counter
 import pickle
 
 from std_msgs.msg import Header
@@ -83,67 +82,44 @@ class FaceRecognition(BaseRecognition):
             while True:
                 rospy.logwarn(f"flatten counte erro {e}")
 
-    def encode_faces(self, face_bboxes = None):
+    def encode_faces(self, facesBbox, image):
+        encodings = []
+        names = []
         try:
+            encoded_face = self.loadVar('features')
+        except:
+            encoded_face = {}
+        train_dir = os.listdir(self.dataset_dir)
 
-            encodings = []
-            names = []
-            try:
-                encoded_face = self.loadVar('features')
-            except:
-                encoded_face = {}
-            train_dir = os.listdir(self.dataset_dir)
+        for person in train_dir:
+            if person not in self.know_faces[0]:   
+                pix = os.listdir(self.dataset_dir + person)
 
-            for person in train_dir:
-                if person not in self.know_faces[0]:   
-                    pix = os.listdir(self.dataset_dir + person)
+                for person_img in pix:
+                    M_face = None
+                    M_area = -float('inf')
+                    for top, right, bottom, left in facesBbox:
+                        area = (bottom - top)*(right - left)
+                        if area > M_area:
+                            M_area = area
+                            M_face = (top, right, bottom, left)
 
-                    for person_img in pix:
-                        if face_bboxes is not None:
-                            face_bbox = face_bboxes.pop(0)
+                    if M_face is not None:
+                        face_enc = face_recognition.face_encodings(image, known_face_locations=[M_face])[0]
+                        encodings.append(face_enc)
 
-                            center_x = face_bbox.center.x
-                            center_y = face_bbox.center.y
-                            size_x = face_bbox.size_x
-                            size_y = face_bbox.size_y
-
-                            left = center_x - size_x / 2
-                            right = center_x + size_x / 2
-                            top = center_y + size_y / 2
-                            bottom = center_y - size_y / 2
-
-                            face_bounding_boxes.append([top, right, bottom, left])
-            
+                        if person not in names:
+                            names.append(person)
+                            encoded_face[person] = []
+                            encoded_face[person].append(face_enc)
                         else:
-                            face = face_recognition.load_image_file(self.dataset_dir + person + "/" + person_img)
-                            face_bounding_boxes = face_recognition.face_locations(face, model = 'yolov8')
+                            encoded_face[person].append(face_enc)
+                    else:
+                        print(person + "/" + person_img + " was skipped and can't be used for training")
+            else:
+                pass
+        self.saveVar(encoded_face, 'features')             
 
-                        M_face = None
-                        M_area = -float('inf')
-                        for top, right, bottom, left in face_bounding_boxes:
-                            area = (bottom - top)*(right - left)
-                            if area > M_area:
-                                M_area = area
-                                M_face = (top, right, bottom, left)
-
-                        if M_face is not None:
-                            face_enc = face_recognition.face_encodings(face, known_face_locations=[M_face])[0]
-                            encodings.append(face_enc)
-
-                            if person not in names:
-                                names.append(person)
-                                encoded_face[person] = []
-                                encoded_face[person].append(face_enc)
-                            else:
-                                encoded_face[person].append(face_enc)
-                        else:
-                            print(person + "/" + person_img + " was skipped and can't be used for training")
-                else:
-                    pass
-            self.saveVar(encoded_face, 'features')             
-        except KeyError as e:
-            while True:
-                rospy.logwarn(f"encode counte erro {e}")
     def PeopleIntroducing(self, ros_srv):
         face_bboxes = []
         name = ros_srv.name
@@ -182,28 +158,41 @@ class FaceRecognition(BaseRecognition):
         num_images = ros_srv.num_images
 
         i = 0
+        facesBbox = []
         while i < num_images:
             self.regressiveCounter(ros_srv.interval)
             try:
-                recognized_faces = rospy.wait_for_message('/butia_vision/br/face_recognition', Recognitions2D, 1000)
+                faceMessage = rospy.wait_for_message('/butia_vision/br/face_recognition', Recognitions2D, 1000)
+
+                image = faceMessage.image_rgb
+
+                ros_image = ros_numpy.numpify(image)
+                ros_image = np.flip(ros_image)
+                ros_image = np.flipud(ros_image)
             except (Exception) as e:
                 break
-            for face in recognized_faces:
-                if face.label == 'unknown':
-                    ros_image = cv2.cvtColor(face.mask, cv2.COLOR_BGR2RGB)
-                    cv2.imwrite(os.path.join(NAME_DIR, add_image_labels[i]), ros_image)
-                    rospy.logwarn('Picture ' + add_image_labels[i] + ' was  saved.')
-                    i+= 1
-                    face_bboxes.append(face.bbox)
-                    break
+            for faceInfos in faceMessage.descriptions:
+    
+                    if faceInfos.label == 'unknown':
+    
+                        bbox = faceInfos.bbox
+                        facesBbox.append([bbox.center.y - int(bbox.size_y/2),bbox.center.y + int(bbox.size_y/2), bbox.center.x - int(bbox.size_x/2),bbox.center.x + int(bbox.size_x/2)])
+                
+            if len(facesBbox) > 0:
+                ros_image = cv2.cvtColor(ros_image, cv2.COLOR_BGR2RGB)
+                cv2.imwrite(os.path.join(NAME_DIR, add_image_labels[i]), ros_image)
+                rospy.logwarn('Picture ' + add_image_labels[i] + ' was  saved.')
+                i+= 1
             else:
                 rospy.logerr("The face was not detected.")
+
 
         cv2.destroyAllWindows()
         response = PeopleIntroducingResponse()
         response.response = True
 
-        self.encode_faces(face_bboxes)
+        self.encode_faces(facesBbox, ros_image)
+
 
         known_faces_dict = self.loadVar('features')
         self.know_faces = self.flatten(known_faces_dict)
@@ -263,7 +252,6 @@ class FaceRecognition(BaseRecognition):
                 description.bbox.center.y = int(top) + int(size[0]/2)
                 description.bbox.size_x = bottom-top
                 description.bbox.size_y = right-left
-                description.mask = ros_numpy.numpify(img)
                 cv2.rectangle(debug_img, (left, top), (right, bottom), (0, 255, 0), 2)
                 
                 font = cv2.FONT_HERSHEY_DUPLEX
@@ -272,6 +260,8 @@ class FaceRecognition(BaseRecognition):
 
                 face_rec.descriptions.append(description)
        
+                face_rec.image_rgb = img
+                
             self.debug_publisher.publish(ros_numpy.msgify(Image, debug_img, 'bgr8'))
             if len(face_rec.descriptions) > 0:
                 self.face_recognition_publisher.publish(face_rec)
