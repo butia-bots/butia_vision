3b29edb95df2e34eb53b987a9a0a4fa433613b28
Eduardo Evangelista <eduardo.evangelista2002@gmail.com>
2024-10-21 15:24:56 -0300

    :sparkles: feat: add deep face into face recognition

--- butia_recognition/scripts/butia_recognition/face_recognition/face_recog.py
+++ butia_recognition/scripts/butia_recognition/face_recognition/face_recog.py
@@ -10,35 +10,59 @@ import numpy as np
 import os
 from copy import copy
 import cv2
-import face_recognition
 import time
 import rospkg
 import pickle
+from deepface import DeepFace
+
 
 from std_msgs.msg import Header
 from sensor_msgs.msg import Image
 from butia_vision_msgs.msg import Description2D, Recognitions2D
 from butia_vision_msgs.srv import PeopleIntroducing, PeopleIntroducingResponse
 from geometry_msgs.msg import Vector3
+from cv_bridge import CvBridge
+
+import os
+
+import sys
+bridge = CvBridge()
 
 PACK_DIR = rospkg.RosPack().get_path('butia_recognition')
+
+class SuppressOutput:
+    def __enter__(self):
+        self._stdout = sys.stdout
+        self._stderr = sys.stderr
+        sys.stdout = open(os.devnull, 'w')
+        sys.stderr = open(os.devnull, 'w')
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        sys.stdout.close()  # Fechar o arquivo devnull
+        sys.stderr.close()
+        sys.stdout = self._stdout
+        sys.stderr = self._stderr
+
 class FaceRecognition(BaseRecognition):
     def __init__(self, state=True):
         super().__init__(state=state)
-        dataset_dir = os.path.join(PACK_DIR, 'dataset')
-        self.features_dir = os.path.join(dataset_dir, 'features')
-        self.dataset_dir = os.path.join(dataset_dir, 'people/')
+        self.dataset_dir = os.path.join(PACK_DIR, 'dataset')
+        os.makedirs(self.dataset_dir, exist_ok=True)
+
+        # Create a test directory inside the dataset directory with a single black image
+        test_dir = os.path.join(self.dataset_dir, 'test')
+        os.makedirs(test_dir, exist_ok=True)
+        cv2.imwrite(os.path.join(test_dir, 'black.jpg'), np.zeros((1, 1, 3), dtype=np.uint8))
+        rospy.loginfo(f'Test directory created at {test_dir} with a black image.')
+        
         self.readParameters()
-
         self.initRosComm()
-
-        known_faces_dict = self.loadVar('features')
-        self.know_faces = self.flatten(known_faces_dict)
 
     def initRosComm(self):
         self.debug_publisher = rospy.Publisher(self.debug_topic, Image, queue_size=self.debug_qs)
         self.face_recognition_publisher = rospy.Publisher(self.face_recognition_topic, Recognitions2D, queue_size=self.face_recognition_qs)
-        self.introduct_person_service = rospy.Service(self.introduct_person_servername, PeopleIntroducing, self.PeopleIntroducing) 
+        self.introduct_person_service = rospy.Service(self.introduct_person_servername, PeopleIntroducing, self.PeopleIntroducing)
+        self.image_subscriber = rospy.Subscriber('/usb_cam/image_raw', Image, self.callback)
 
         super().initRosComm(callbacks_obj=self)
         rospy.loginfo('foi 2')
@@ -46,83 +70,16 @@ class FaceRecognition(BaseRecognition):
     def regressiveCounter(self, sec):
         sec = int(sec)
         for i  in range(0, sec):
-            rospy.logwarn(str(sec-i) + '...')
+            print(str(sec-i) + '...')
             time.sleep(1)
-    
-    def saveVar(self, variable, filename):
-        try:
-            with open(self.features_dir + '/' +  filename + '.pkl', 'wb') as file:
-                pickle.dump(variable, file)
-        except KeyError as e:
-            while True:
-                rospy.logwarn(f"Save var counte erro {e}")
-
-    def loadVar(self, filename):
-        try:
-            file_path = self.features_dir + '/' +  filename + '.pkl'
-            if os.path.exists(file_path):
-                with open(file_path, 'rb') as file:
-                    variable = pickle.load(file)
-                return variable
-            return {}
-        except KeyError as e:
-            while True:
-                rospy.logwarn(f"LoadVar counte erro {e}")
-
-    def flatten(self, l):
-        try:
-            values_list = [item for sublist in l.values() for item in sublist]
-            keys_list = [item for name in l.keys() for item in [name]*len(l[name])]
-            return keys_list, values_list
-        except KeyError as e:
-            while True:
-                rospy.logwarn(f"flatten counte erro {e}")
-
-    def encode_faces(self, facesBbox, image):
-        encodings = []
-        names = []
-        try:
-            encoded_face = self.loadVar('features')
-        except:
-            encoded_face = {}
-        train_dir = os.listdir(self.dataset_dir)
-
-        for person in train_dir:
-            if person not in self.know_faces[0]:   
-                pix = os.listdir(self.dataset_dir + person)
-
-                for person_img in pix:
-                    M_face = None
-                    M_area = -float('inf')
-                    for top, right, bottom, left in facesBbox:
-                        area = (bottom - top)*(right - left)
-                        if area > M_area:
-                            M_area = area
-                            M_face = (top, right, bottom, left)
-
-                    if M_face is not None:
-                        face_enc = face_recognition.face_encodings(image, known_face_locations=[M_face])[0]
-                        encodings.append(face_enc)
-
-                        if person not in names:
-                            names.append(person)
-                            encoded_face[person] = []
-                            encoded_face[person].append(face_enc)
-                        else:
-                            encoded_face[person].append(face_enc)
-                    else:
-                        print(person + "/" + person_img + " was skipped and can't be used for training")
-            else:
-                pass
-        self.saveVar(encoded_face, 'features')             
 
     def PeopleIntroducing(self, ros_srv):
-        face_bboxes = []
         name = ros_srv.name
         num_images = ros_srv.num_images
+
         NAME_DIR = os.path.join(self.dataset_dir, name)
         os.makedirs(NAME_DIR, exist_ok=True)
-        os.makedirs(self.features_dir, exist_ok=True)
+
         image_type = '.jpg'
 
         image_labels = os.listdir(NAME_DIR)
@@ -132,21 +89,26 @@ class FaceRecognition(BaseRecognition):
         j = num_images
         number = [] 
         for label in image_labels:
+
             number.append(int(label.replace(image_type, '')))
         
         number.sort()
         n = 1
         while j > 0:
+
             if k < len(number):
                 n = number[k] + 1
                 if number[k] == i:
+
                     k += 1
                 else:
+
                     add_image_labels.append((str(i) + image_type))
                     j -= 1      
                 i += 1 
 
             else:
+
                 add_image_labels.append(str(n) + image_type)
                 j -= 1
                 n += 1
@@ -155,113 +117,131 @@ class FaceRecognition(BaseRecognition):
 
         i = 0
         facesBbox = []
+        #Iterate over the number of images to be taken
         while i < num_images:
+
             self.regressiveCounter(ros_srv.interval)
-            try:
-                faceMessage = rospy.wait_for_message('/butia_vision/br/face_recognition', Recognitions2D, 1000)
 
-                image = faceMessage.image_rgb
+            faceMessage = rospy.wait_for_message('/butia_vision/br/face_recognition', Recognitions2D, 1000)
+            image = faceMessage.image_rgb
+            ros_image = ros_numpy.numpify(image)
 
-                ros_image = ros_numpy.numpify(image)
-                ros_image = np.flip(ros_image)
-                ros_image = np.flipud(ros_image)
-            except (Exception) as e:
-                break
+            croped_face_image = None
+            ros_image_cv = cv2.cvtColor(ros_image, cv2.COLOR_RGB2BGR)
             for faceInfos in faceMessage.descriptions:
-    
-                    if faceInfos.label == 'unknown':
-    
+                    if faceInfos.label == 'unknown' or faceInfos.label == name:
                         bbox = faceInfos.bbox
-                        facesBbox.append([int(bbox.center.y - int(bbox.size_y/2)),int(bbox.center.y + int(bbox.size_y/2)), int(bbox.center.x - int(bbox.size_x/2)),int(bbox.center.x + int(bbox.size_x/2))])
-            if len(facesBbox) > 0:
-                ros_image = cv2.cvtColor(ros_image, cv2.COLOR_BGR2RGB)
-                cv2.imwrite(os.path.join(NAME_DIR, add_image_labels[i]), ros_image)
+                        biggest_area = 0
+                        detected_area = bbox.size_x * bbox.size_y
+                        if detected_area > biggest_area:
+                            biggest_area = detected_area
+
+                            left = int(bbox.center.x - bbox.size_x / 2)
+                            right = int(bbox.center.x + bbox.size_x / 2)
+                            top = int(bbox.center.y + bbox.size_y / 2)
+                            bottom = int(bbox.center.y - bbox.size_y / 2)
+
+                            croped_face_image = ros_image_cv[
+                                bottom:top,
+                                left:right
+                            ]
+                            
+            if croped_face_image is not None:
+                cv2.imwrite(os.path.join(NAME_DIR, add_image_labels[i]), croped_face_image)
                 rospy.logwarn('Picture ' + add_image_labels[i] + ' was  saved.')
                 i+= 1
             else:
                 rospy.logerr("The face was not detected.")
 
-
         cv2.destroyAllWindows()
         response = PeopleIntroducingResponse()
         response.response = True
 
-        self.encode_faces(facesBbox, ros_image)
-
-
-        known_faces_dict = self.loadVar('features')
-        self.know_faces = self.flatten(known_faces_dict)
         return response
 
-    @ifState
-    def callback(self, *args):
-        try:
-            thold = 0.5
-            face_rec = Recognitions2D()
-            source_data = self.sourceDataFromArgs(args)
+    def callback(self, args):
+        thold = 0.5
+        names = []
+        h = Header()
+        h.seq = self.seq
+        self.seq += 1
+        h.stamp = rospy.Time.now()
+        face_rec = Recognitions2D()
+        face_rec.header = h
+        ros_img_small_frame = ros_numpy.numpify(args)
 
-            if 'image_rgb' not in source_data:
-                rospy.logwarn('Souce data has no image_rgb.')
-                return None
+        with SuppressOutput():
+            current_faces = DeepFace.extract_faces(
+                ros_img_small_frame,
+                detector_backend='yolov8',
+                enforce_detection=False
+            )
+
+        debug_img = copy(ros_img_small_frame)
+        for idx in range(len(current_faces)):
+
+            result = current_faces[idx]
+            face = (result['face']* 255).astype('uint8')
+
+            x_top = result['facial_area']['x']
+            y_top = result['facial_area']['y']
+            w = result['facial_area']['w']
+            h = result['facial_area']['h']
+
+            x_center = x_top + int(w/2)
+            y_center = y_top + int(h/2)
+
+            left = x_center - int(w/2)
+            right = x_center + int(w/2)
+            top = y_center + int(h/2)
+            bottom = y_center - int(h/2)
+
+            description = Description2D()
+            name = 'unknown'
+
+            #If there are known faces, compare the current face with them
+            with SuppressOutput():
+                detected_face_result = DeepFace.find(
+                    img_path=face,
+                    db_path=self.dataset_dir,
+                    model_name='Facenet512',
+                    detector_backend='yolov8',
+                    enforce_detection=False,
+                    threshold=thold
+                )
+
+            if detected_face_result[0].empty:
+                pass
+            else:
+                final_image_path = detected_face_result[0].iloc[0]['identity']
+                name = final_image_path.split("/")[-2]
+
+            description.label = name
+            names.append(name)
+
+            description_header = args.header
+            description_header.seq = 0
+            description.header = copy(description_header)
+            description.type = Description2D.DETECTION
+            description.id = description.header.seq
+            description.score = 1
+            description.max_size = Vector3(*[0.2, 0.2, 0.2])
+            description.bbox.center.x = x_center
+            description.bbox.center.y = y_center
+            description.bbox.size_x = w
+            description.bbox.size_y = h
+
+
+            cv2.rectangle(debug_img, (left, top), (right, bottom), (0, 255, 0), 2)
+            cv2.putText(debug_img, name, (left + 4, bottom - 4), cv2.FONT_HERSHEY_DUPLEX, 0.5, (0,0,255), 2)
+            description_header.seq += 1
+            face_rec.descriptions.append(description)
+            face_rec.image_rgb = args
             
-            img = source_data['image_rgb']
-            h = Header()
-            h.seq = self.seq
-            self.seq += 1
-            h.stamp = rospy.Time.now()
-            face_rec.header = h
-            face_rec = BaseRecognition.addSourceData2Recognitions2D(source_data, face_rec)
-            #rospy.loginfo('Image ID: ' + str(img.header.seq))
+        self.debug_publisher.publish(ros_numpy.msgify(Image, debug_img, 'rgb8'))
 
-            ros_img_small_frame = ros_numpy.numpify(img)
-            current_faces = face_recognition.face_locations(ros_img_small_frame, model = 'yolov8')
-            current_faces_encodings = face_recognition.face_encodings(ros_img_small_frame, current_faces)
-            debug_img = copy(ros_img_small_frame)
-            names = []
-            name_distance=[]
-            for idx in range(len(current_faces_encodings)):
-                current_encoding = current_faces_encodings[idx]
-                top, right, bottom, left = current_faces[idx]
-                description = Description2D()
-                name = 'unknown'
-                if(len(self.know_faces[0]) > 0):
-                    face_distances = np.linalg.norm(self.know_faces[1] - current_encoding, axis = 1)
-                    min_distance_idx = np.argmin(face_distances)
-                    min_distance = face_distances[min_distance_idx]
-                    if min_distance < thold:
-                        name = (self.know_faces[0][min_distance_idx])
-                description.label = name
-
-                names.append(name)
-
-                description_header = img.header
-                description_header.seq = 0
-                description.header = copy(description_header)
-                description.type = Description2D.DETECTION
-                description.id = description.header.seq
-                description.score = 1
-                description.max_size = Vector3(*[0.2, 0.2, 0.2])
-                size = int(right-left), int(bottom-top)
-                description.bbox.center.x = int(left) + int(size[1]/2)
-                description.bbox.center.y = int(top) + int(size[0]/2)
-                description.bbox.size_x = bottom-top
-                description.bbox.size_y = right-left
-                cv2.rectangle(debug_img, (left, top), (right, bottom), (0, 255, 0), 2)
-                
-                font = cv2.FONT_HERSHEY_DUPLEX
-                cv2.putText(debug_img, name, (left + 4, bottom - 4), font, 0.5, (0,0,255), 2)
-                description_header.seq += 1
-
-                face_rec.descriptions.append(description)
-       
-                face_rec.image_rgb = img
-                
-            self.debug_publisher.publish(ros_numpy.msgify(Image, debug_img, 'bgr8'))
-            if len(face_rec.descriptions) > 0:
-                self.face_recognition_publisher.publish(face_rec)
-        except KeyError as e:
-            while True:
-                rospy.logwarn(f"callback counte erro {e}")
+        if len(face_rec.descriptions) > 0:
+            self.face_recognition_publisher.publish(face_rec)
 
     def readParameters(self):
         self.debug_topic = rospy.get_param("~publishers/debug/topic", "/butia_vision/br/debug")
